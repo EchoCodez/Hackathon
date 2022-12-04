99b823e13e94959129e4ff950df1695f6a5a6781
EchoCodez <mihir.nimkar@gmail.com>
2022-12-03 21:54:04 -0500

    added gspread

--- infinity.py
+++ infinity.py
@@ -5,7 +5,6 @@ gc = gs.service_account(filename='key.json')
 sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1a3kh0zcaCTJM7blFzkD0S5p1zqyWI-J4wXs4Jl4fMtk/edit?usp=sharing')
 
 ws = sh.worksheet('Form Responses 1')
-df = pd.DataFrame(ws.get_all_records())
-df.head()
-
-print(df)
+for i in ws.get_all_records():
+    if i.get("What is your budget?") < 10:
+        print("Hello")
